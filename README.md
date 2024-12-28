799763aa9f3790322591c9fe69e2d8c49dad0680
JeremyDufeux <jeremy.dufeux@gmail.com>
2024-12-28 19:14:09 +0100

    Add launch configuration, update package version, and enhance README
    - Introduced launch configuration for extension development.
    - Updated package version to 0.1.0 and adjusted VS Code engine compatibility.
    - Removed outdated snippet template and added detailed contribution guidelines in README.
    - Implemented functions hover and completion features for improved API documentation.

--- README.md
+++ README.md
@@ -1,8 +1,45 @@
-# ma3-lua-api README
 
-VS Code extension for the Grand Ma 3 Lua Api, it provide autocomplete and documentation for functions to write Grand Ma 3 plugins.
+# Ma3 lua API
+This is a VS Code extension for the Grand Ma 3 Lua Api, it provide autocomplete and documentation for functions to write Grand Ma 3 plugins.
 
-## Contribute
+![insertion](images/insertion.gif)
 
+![documentation](images/documentation.gif)
+
+
+## How to contribute
 This is an open source project, feel free to contribute by making pull requests
 
+### What do you need:
+
+ - [VS Code IDE](https://code.visualstudio.com/download)
+ - [Node JS](https://nodejs.org/en/download)
+ - Clone this repository 
+
+### To update or complete the API documentation:
+
+This image explain how to fill the json file that contain all documentation in the resources folder
+![json filling](images/json_filling.png)
+
+For the body part, fill the parentheses with the function parameters like:
+
+    function(${1:fixtureId}, ${2:count}, ${3:type})
+
+All the text part need to be converted to Markdown format, this can be done using this online tool: [Clipboard 2 Markdown](https://euangoddard.github.io/clipboard2markdown/)
+
+After that you have to convert the markdown text to a single line string, this can be done with [json Stringlfy](https://toolsaday.com/text-tools/json-stringify)
+
+For the code part of the example, you only have to convert to a single line string.
+
+Additionally you have update the [ma3_documented_api.lua](resources/ma3_documented_api.lua) file in the resources folder
+This file contain dummy functions with input parameters, you have to enumerate all parameters types, if they are optional, if they can be nil and the return type:
+
+    ---@param fixtureTableHandle Handle
+    ---@param multiPatchAmount integer
+    ---@param undoText string @Optional
+    ---@return integer|nil
+    function CreateMultiPatch(fixtureTableHandle, multiPatchAmount, undoText)
+        return 0
+    end 
+
+Test your modification and then, start a pull request!
--- src/extension.js
+++ src/extension.js
@@ -3,49 +3,122 @@ const path = require('path');
 const fs = require('fs');
 
 function activate(context) {
+    AddApiToWorkspace(context)
+
     const parentDir = path.dirname(__dirname);
-    const snippetsPath = path.join(parentDir, 'snippets', 'lua.json');
+    const objectFreeApiPath = path.join(parentDir, 'resources', 'ma3-object-free-api.json');
     
-    fs.readFile(snippetsPath, 'utf8', (err, data) => {
+    fs.readFile(objectFreeApiPath, 'utf8', (err, data) => {
         if (err) {
             console.error('Error reading file lua.json:', err);
             return;
         }
 
-        const snippets = JSON.parse(data);
-        const globals = Object.keys(snippets); 
+        AddFunctionsHover(context, data)
+        AddFunctionsCompletion(data)    
+    });
+}
 
-        vscode.workspace.getConfiguration('Lua.diagnostics').update('globals', globals, vscode.ConfigurationTarget.Workspace);
-        
-        console.log('Globals update succes:', globals);
-        
+function AddApiToWorkspace(context){
+    const luaLibraryPath = path.join(context.extensionPath, 'resources', 'ma3_documented_api.lua');
+    const config = vscode.workspace.getConfiguration('Lua');
+    let currentLibraries = config.get('workspace.library') || [];
+
+    if (!Array.isArray(currentLibraries)) {
+        currentLibraries = [];
+    }
+
+    if (!currentLibraries.includes(luaLibraryPath)) {
+        currentLibraries.push(luaLibraryPath);
+        config.update('workspace.library', currentLibraries, vscode.ConfigurationTarget.Global);
+    }
+}
+
+function AddFunctionsHover(context, data){
     
+    const objectFreeJson = JSON.parse(data);
+    const objectFreeKeys = Object.keys(objectFreeJson); 
         context.subscriptions.push(vscode.languages.registerHoverProvider('lua', {
             provideHover(document, position, token) {
                 const range = document.getWordRangeAtPosition(position);
-                const word = document.getText(range);
+                if (!range) return;
 
-                if (globals.includes(word) && snippets[word]) {
-                    const description = snippets[word].description;
-                    return new vscode.Hover({
-                        language: 'lua',
-                        value: `
-\`\`\`
-${description}
-\`\`\`
-                    `
-                    });
+                const snippetKey = document.getText(range);
+                
+                const markdownContent = new vscode.MarkdownString();
+                markdownContent.isTrusted = true;
+
+                if (objectFreeKeys.includes(snippetKey) && objectFreeJson[snippetKey]) {
+                    const snippetData = objectFreeJson[snippetKey];
+                    const cleanedPrefix = snippetData.prefix.replace(/\(.*?\)/g, '');
+
+                     markdownContent.appendMarkdown(`# ${cleanedPrefix}\n\n`);
+                    
+                    
+                    markdownContent.appendMarkdown(`${snippetData.description}\n\n`);
+
+                    if (snippetData.examples) {
+                        const examples = snippetData.examples;
+                        const examplesCount = Object.keys(examples).length;
+
+                        for (const key in examples) {
+                            const example = examples[key];
+                            if (examplesCount > 1) {
+                                markdownContent.appendMarkdown(`\n\nExample ${key}\n-------\n\n`);
+                            } else {
+                                markdownContent.appendMarkdown(`\n\nExample\n-------\n\n`);
+                            }
+                            markdownContent.appendMarkdown(example.description);
+                            markdownContent.appendCodeblock(example.code, 'lua');
+                            markdownContent.appendMarkdown(`\n\n${example.suffix}`);
+                        }
+                    }
+
+                    markdownContent.appendMarkdown(`${snippetData.suffix}\n\n`);
+                    return new vscode.Hover(markdownContent);
                 }
 
                 return null;
             }
         }));
+}
 
-        context.subscriptions.push(hoverProvider);
+function AddFunctionsCompletion(data){
+    const objectFreeJson = JSON.parse(data);
+
+    const snippetProvider = vscode.languages.registerCompletionItemProvider('lua', {
+        provideCompletionItems() {
+            return Object.entries(objectFreeJson).map(([funcName, data]) => {
+                const item = new vscode.CompletionItem(funcName, vscode.CompletionItemKind.Snippet);
+                item.insertText = new vscode.SnippetString(data.body[0]);
+                item.kind = vscode.CompletionItemKind.Function;
+                item.sortText = "0";
+                item.detail = "grandMA3 API";
+                
+        item.label = { 
+            label: funcName,
+            description: "grandMa3 API",
+        };
+                return item;
+            });
+        }
     });
 }
 
-function deactivate() {}
+function removeApiFromWorkspace(){
+    const luaLibraryPath = path.join(context.extensionPath, 'resources', 'ma3_documented_api.lua');
+    const config = vscode.workspace.getConfiguration('Lua');
+    let currentLibraries = config.get('workspace.library') || [];
+
+    if (Array.isArray(currentLibraries)) {
+        currentLibraries = currentLibraries.filter(path => path !== luaLibraryPath);
+        config.update('workspace.library', currentLibraries, vscode.ConfigurationTarget.Global);
+    }
+}
+
+function deactivate() {
+    removeApiFromWorkspace()
+}
 
 module.exports = {
     activate,
